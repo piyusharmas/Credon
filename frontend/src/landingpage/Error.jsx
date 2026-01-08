236c01bb4fc68f342e0eed0ea2bd2fcc9a1136e0
piyusharmas <piyusharmas2005@gmail.com>
2026-01-08 21:06:50 +0530

    did a few changes in error file

--- frontend/src/landingpage/Error.jsx
+++ frontend/src/landingpage/Error.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './Navbar';
+import NavBar from './Navbar';
 import Footer from './Footer';
 
 function ErrorPage() {
     return (
         <>
-        <Navbar/>
+        <NavBar/>
         <div className='container p-5 mb-5'>
             <div className='row text-center'>
                 <h1 className='mt-5'>Error:404</h1>
